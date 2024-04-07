a085d736d235255edcded3ce5b1367c66a62bcb2
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-04-07 14:49:31 +0530

    basic interaction

---
+++ Basic Interactions copy/Basic Interactions/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Basic Interactions
+//
+//
+//  Created by Prince Vasoya on 07/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func setText(_ sender: Any) {
+        label.text = textField.text
+    }
+    
+    @IBAction func clearText(_ sender: Any) {
+        label.text = ""
+    }
+    
+}
+
