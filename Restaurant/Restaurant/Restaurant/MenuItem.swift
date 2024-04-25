f5d50140057b753111111f768ce613d7c62b5986
vasoyaprince14 <vasoyaprince14@gmail.com>
2024-04-25 15:09:14 +0530

    restaurant

---
+++ Restaurant/Restaurant/Restaurant/MenuItem.swift
@@ -0,0 +1,42 @@
+//
+//  MenuItem.swift
+//  Restaurant
+//
+//  Created by Prince vasoya
+//
+
+import Foundation
+
+struct MenuItem: Codable {
+    var id: Int
+    var name: String
+    var description: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+}
+
+struct MenuItems: Codable {
+    let items: [MenuItem]
+}
+
+struct Categories: Codable {
+    let categories: [String]
+}
+
+struct PreparationTime: Codable {
+    let prepTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "preparation_time"
+    }
+}
---
+++ Restaurant/Restaurant/Restaurant/OrderConfirmationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OrderConfirmationViewController.swift
+//  Restaurant
+//
+//  Created by Prince vasoya
+//
+
+import UIKit
+
+class OrderConfirmationViewController: UIViewController {
+    
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    
+    var minutes: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timeRemainingLabel.text = "Thank you for your order! Your wait time is approximately \(minutes!) minutes."
+    }
+}
